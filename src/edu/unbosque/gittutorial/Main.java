99446a3febdfd8c1a2c78f3da863302981cf14fa
Camilo Gomez <fcgomez@unbosque.edu.co>
2021-02-17 19:31:08 -0500

    .

--- src/edu/unbosque/gittutorial/Main.java
+++ src/edu/unbosque/gittutorial/Main.java
@@ -1,7 +1,12 @@
 package edu.unbosque.gittutorial;
 
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
+        Date currentDate = new Date();
+
         System.out.println("Hello world! Estamos en el curso de Git y Github");
+        System.out.println("The current date is " + currentDate.toString());
     }
 }
